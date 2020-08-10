b6f909d19d8b8fadb057c297f5dbf0149c7d09db
Ian Chou <ianchou19@gmail.com>
2020-08-10 10:46:27 -0400

    Social Clone Part Eleven to The End

--- simplesocial/posts/admin.py
+++ simplesocial/posts/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 
 from . import models
 
-
-class GroupMemberInline(admin.TabularInline):
-    model = models.GroupMember
-
-
 admin.site.register(models.Group)
--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,4 +1,4 @@
-# from django.contrib import messages
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
